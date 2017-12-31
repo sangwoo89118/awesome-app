39ce582016ada7d94fdb4c73c34574845162d689
sangwoo89118 <sangwoo89118@gmail.com>
2017-12-31 12:52:17 -0800

    finished react setup

---
+++ client/src/components/get_data.js
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react';
+
+export default class GetData extends Component{
+
+
+    render(){
+        return(
+            <div>
+                <h1>This is get data!</h1>
+            </div>
+        )
+    }
+}
